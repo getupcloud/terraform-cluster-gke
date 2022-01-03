39c1e7efeeada07f59eaa16243f21f9bf5b66175
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-01-03 11:43:59 -0300

    Fix cluster type

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "flux" {
   source = "github.com/getupcloud/terraform-module-flux?ref=main"
 
   git_repo       = var.flux_git_repo
-  manifests_path = "./clusters/${var.name}/generic/manifests"
+  manifests_path = "./clusters/${var.name}/gke/manifests"
   wait           = var.flux_wait
   manifests_template_vars = merge({
     cronitor_id : module.cronitor.cronitor_id
@@ -18,7 +18,7 @@ module "cronitor" {
 
   cluster_name  = var.name
   customer_name = var.customer_name
-  suffix        = "generic"
+  suffix        = "gke"
   tags          = []
   api_key       = var.cronitor_api_key
   pagerduty_key = var.cronitor_pagerduty_key
