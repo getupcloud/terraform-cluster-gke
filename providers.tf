ec00ce67748831d14ef05e4d8841349689491cc8
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-02-02 21:37:12 -0300

    add missing providers.tf

---
+++ providers.tf
@@ -0,0 +1,13 @@
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+
+provider "kubectl" {
+  load_config_file       = false
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+  apply_retry_count      = 2
+}
