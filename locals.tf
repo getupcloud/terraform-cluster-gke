2cae63f85eb2bb05eeb4c123a78d49811f244c3a
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-03-16 15:19:40 -0300

    minor changes

--- locals.tf
+++ locals.tf
@@ -1,4 +1,9 @@
 locals {
+  kubeconfig_filename        = abspath(pathexpand(var.kubeconfig_filename))
+  api_endpoint               = "https://${module.gke.endpoint}"
+  token                      = data.google_client_config.default.access_token
+  certificate_authority_data = base64decode(module.gke.ca_certificate)
+
   suffix = random_string.suffix.result
   secret = random_string.secret.result
 }
--- providers.tf
+++ providers.tf
@@ -1,14 +1,14 @@
 provider "kubernetes" {
-  config_path            = var.use_kubeconfig ? var.kubeconfig_filename : null
-  host                   = var.use_kubeconfig ? null : "https://${module.gke.endpoint}"
-  token                  = var.use_kubeconfig ? null : data.google_client_config.default.access_token
-  cluster_ca_certificate = var.use_kubeconfig ? null : base64decode(module.gke.ca_certificate)
+  config_path            = var.use_kubeconfig ? local.kubeconfig_filename : null
+  host                   = var.use_kubeconfig ? null : local.api_endpoint
+  token                  = var.use_kubeconfig ? null : local.token
+  cluster_ca_certificate = var.use_kubeconfig ? null : local.certificate_authority_data
 }
 
 provider "kubectl" {
-  load_config_file       = var.use_kubeconfig
-  host                   = var.use_kubeconfig ? null : "https://${module.gke.endpoint}"
-  token                  = var.use_kubeconfig ? null : data.google_client_config.default.access_token
-  cluster_ca_certificate = var.use_kubeconfig ? null : base64decode(module.gke.ca_certificate)
   apply_retry_count      = 2
+  load_config_file       = var.use_kubeconfig
+  host                   = var.use_kubeconfig ? null : local.api_endpoint
+  token                  = var.use_kubeconfig ? null : local.token
+  cluster_ca_certificate = var.use_kubeconfig ? null : local.certificate_authority_data
 }
