5cb95172b066c350123069f9ec6fdcd497666bd2
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-05-20 15:24:32 -0300

    bump

--- main.tf
+++ main.tf
@@ -46,7 +46,8 @@ module "teleport-agent" {
 data "google_client_config" "default" {}
 
 module "gke" {
-  source = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
+  source  = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
+  version = "20.0.0"
 
   configure_ip_masq             = var.configure_ip_masq
   default_max_pods_per_node     = var.default_max_pods_per_node
